76ac76df0dc82fbc32e35d1a8b63c40ffa5c185b
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-05 12:58:56 +0530

    feat: Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ UniWorkshop/src/main/java/com/system/UniWorkshop/UniWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.system.UniWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniWorkshopApplication.class, args);
+	}
+
+}
---
+++ UniWorkshop/src/test/java/com/system/UniWorkshop/UniWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.UniWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
